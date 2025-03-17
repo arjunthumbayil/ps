cff535c9118a424614b0fee263d7a016fa16b68f
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-03-17 22:34:14 +0530

    Solved 1

--- 1_rotate_array.js
+++ 1_rotate_array.js
@@ -1,56 +1,61 @@
 const testCases = [
   {
-    "input_args": {
-      "array": [1, 2, 3, 4],
-      "shift": 1
+    input_args: {
+      array: [1, 2, 3, 4],
+      shift: 1,
     },
-    "expected_output": [4, 1, 2, 3],
+    expected_output: [4, 1, 2, 3],
   },
   {
-    "input_args": {
-      "array": [1, 2, 3, 4, 5, 6],
-      "shift": 2
+    input_args: {
+      array: [1, 2, 3, 4, 5, 6],
+      shift: 2,
     },
-    "expected_output": [5, 6, 1, 2, 3, 4],
+    expected_output: [5, 6, 1, 2, 3, 4],
   },
   {
-    "input_args": {
-      "array": [1, 2, 3, 4],
-      "shift": 3
+    input_args: {
+      array: [1, 2, 3, 4],
+      shift: 3,
     },
-    "expected_output": [2, 3, 4, 1],
+    expected_output: [2, 3, 4, 1],
   },
   {
-    "input_args": {
-      "array": [1, 2, 3, 4],
-      "shift": 0
+    input_args: {
+      array: [1, 2, 3, 4],
+      shift: 0,
     },
-    "expected_output": [1, 2, 3, 4],
+    expected_output: [1, 2, 3, 4],
   },
   {
-    "input_args": {
-      "array": [1],
-      "shift": 0
+    input_args: {
+      array: [1],
+      shift: 0,
     },
-    "expected_output": [1],
+    expected_output: [1],
   },
   {
-    "input_args": {
-      "array": [1, 2],
-      "shift": 1
+    input_args: {
+      array: [1, 2],
+      shift: 1,
     },
-    "expected_output": [2, 1],
-  }
-]
+    expected_output: [2, 1],
+  },
+];
 
 function shiftArray(array, shift) {
   // your code here
+
+  let rightPart = array.slice(array.length - shift);
+  let leftPart = array.slice(0, array.length - shift);
+  let result = rightPart.concat(leftPart);
+  return result;
 }
 
 for (const testCase of testCases) {
   const { input_args, expected_output } = testCase;
   const result = shiftArray(input_args.array, input_args.shift);
 
-  console.log("\n\n")
-  console.log({ input_args, result, expected_output })
+  console.log('\n\n');
+  console.log({ input_args, result, expected_output });
 }
---
+++ 2_test.js
@@ -0,0 +1,7 @@
+let apps = ['Audible', 'Settings', 'Spotify', 'Task Manager', 'Weather'];
+
+let query = 'st';
+
+function appSearch(apps, query) {
+  const lowerQuery = query.toLowerCase();
+}
