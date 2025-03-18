ff5739e7f97d13047a5ebdab91d2afd6967f838c
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-03-18 17:08:51 +0530

    Solved 2

--- 2_app_search.js
+++ 2_app_search.js
@@ -1,61 +1,79 @@
-const testCases = [{
-  "input_args": {
-    "apps": ["Audible", "Settings", "Spotify", "Task Manager", "Weather"],
-    "query": "st"
+const testCases = [
+  {
+    input_args: {
+      apps: ['Audible', 'Settings', 'Spotify', 'Task Manager', 'Weather'],
+      query: 'st',
+    },
+    expected_output: ['Settings', 'Spotify'],
   },
-  "expected_output": ["Settings", "Spotify"],
-},
-{
-  "input_args": {
-    "apps": ["Nike Run Keeper", "Google Podcasts", "Spotify"],
-    "query": "n"
+  {
+    input_args: {
+      apps: ['Nike Run Keeper', 'Google Podcasts', 'Spotify'],
+      query: 'n',
+    },
+    expected_output: ['Nike Run Keeper'],
   },
-  "expected_output": ["Nike Run Keeper"],
-},
-{
-  "input_args": {
-    "apps": ["Nike Run Keeper", "Google Podcasts", "Settings"],
-    "query": "q"
+  {
+    input_args: {
+      apps: ['Nike Run Keeper', 'Google Podcasts', 'Settings'],
+      query: 'q',
+    },
+    expected_output: [],
   },
-  "expected_output": [],
-},
-{
-  "input_args": {
-    "apps": ["Nike Run Keeper", "Google Podcasts", "Settings"],
-    "query": "ee"
+  {
+    input_args: {
+      apps: ['Nike Run Keeper', 'Google Podcasts', 'Settings'],
+      query: 'ee',
+    },
+    expected_output: ['Nike Run Keeper'],
   },
-  "expected_output": ["Nike Run Keeper"],
-},
-{
-  "input_args": {
-    "apps": ["Nike Run Keeper", "Google Podcasts", "Settings"],
-    "query": "eee"
+  {
+    input_args: {
+      apps: ['Nike Run Keeper', 'Google Podcasts', 'Settings'],
+      query: 'eee',
+    },
+    expected_output: ['Nike Run Keeper'],
   },
-  "expected_output": ["Nike Run Keeper"],
-},
-{
-  "input_args": {
-    "apps": ["Nike Run Keeper", "Google Podcasts", "Settings"],
-    "query": "eeeer"
+  {
+    input_args: {
+      apps: ['Nike Run Keeper', 'Google Podcasts', 'Settings'],
+      query: 'eeeer',
+    },
+    expected_output: ['Nike Run Keeper'],
   },
-  "expected_output": ["Nike Run Keeper"],
-},
-{
-  "input_args": {
-    "apps": ["Nike Run Keeper", "Google Podcasts", "Settings"],
-    "query": "eeeee"
+  {
+    input_args: {
+      apps: ['Nike Run Keeper', 'Google Podcasts', 'Settings'],
+      query: 'eeeee',
+    },
+    expected_output: [],
   },
-  "expected_output": [],
-}]
+];
 
 function appSearch(apps, query) {
   // your code here
+  const lowerQuery = query.toLowerCase();
+
+  return apps.filter((app) => {
+    const lowerApp = app.toLowerCase();
+
+    let i = 0;
+    let j = 0;
+
+    while (i < lowerApp.length && j < lowerQuery.length) {
+      if (lowerApp[i] === lowerQuery[j]) {
+        j++;
+      }
+      i++;
+    }
+    return j === lowerQuery.length;
+  });
 }
 
 for (const testCase of testCases) {
   const { input_args, expected_output } = testCase;
-  const result = appSearch(input_args.array, input_args.shift);
+  const result = appSearch(input_args.apps, input_args.query);
 
-  console.log("\n\n")
-  console.log({ input_args, result, expected_output })
+  console.log('\n\n');
+  console.log({ input_args, result, expected_output });
 }
--- 2_test.js
+++ 2_test.js
@@ -4,4 +4,21 @@ let query = 'st';
 
 function appSearch(apps, query) {
   const lowerQuery = query.toLowerCase();
+
+  return apps.filter((app) => {
+    const lowerApp = app.toLowerCase();
+
+    let i = 0;
+    let j = 0;
+
+    while (i < lowerApp.length && j < lowerQuery.length) {
+      if (lowerApp[i] === lowerQuery[j]) {
+        j++;
+      }
+      i++;
+    }
+    return j === lowerQuery.length;
+  });
 }
+
+console.log(appSearch(apps, query));
